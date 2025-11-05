8eaa16dc21c4ac285381aec0277d9625ca3b700e
GabsZ1 <gabrielleboninibarbosa@gmail.com>
2025-11-05 16:47:41 -0300

    criei uma seeder para a instituição também para que eu possa testar o login e que todos também tenham ela no banco

---
+++ database/seeders/InstituicaoSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Instituicao;
+use Illuminate\Support\Facades\Hash;
+
+class InstituicaoSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Instituicao::updateOrCreate(
+            ['email' => 'contato@gmail.com'],
+            [
+                'nome' => 'Instituição Exemplo',
+                'cnpj' => '12.345.678/0001-99',
+                'password' => Hash::make('12345678'), // senha criptografada
+                'telefone' => '(11) 99999-9999',
+                'descricao' => 'Instituição exemplo criada automaticamente pela seeder.',
+                'endereco' => 'Rua das Flores, 123 - São Paulo/SP',
+                'responsavel' => 'Maria da Silva',
+            ]
+        );
+    }
+}

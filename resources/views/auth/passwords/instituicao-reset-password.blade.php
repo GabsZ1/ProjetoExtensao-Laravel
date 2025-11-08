99a33f0b4d1dbf4d9ede29511582d8ce86e39271
Guilherme <guifmoraiis@gmail.com>
2025-11-08 14:34:34 -0300

    corrige botao cadastrar da tela de esqueci minha senha/resetar senha

--- resources/views/auth/passwords/instituicao-reset-password.blade.php
+++ resources/views/auth/passwords/instituicao-reset-password.blade.php
@@ -35,7 +35,7 @@
                         <a class="nav-link" href="{{ route('home') }}" style="color: white;">Entrar</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#formulario-cadastro" style="color: white;">Cadastrar</a>
+                        <a class="nav-link" href="/#formulario-cadastro" style="color: white;">Cadastrar</a>
                     </li>
                 </ul>
             </div>
